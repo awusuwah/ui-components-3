62b613e56b0ff5d8d196a82d4cc95ae09afaadf3
Marko Maric <marko@markomaric.me>
2023-07-05 23:55:07 +0200

    Build now generates the type declarations.

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-import Button from "./button/Button.vue";
-import "../assets/tailwind.css";
-
-export { Button };
--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/components/index.ts"),
+      entry: resolve(__dirname, "src/index.ts"),
       name: "UiComponentsVue3",
       formats: ["es"],
       fileName: (format) => "ui-components-vue3.mjs",
