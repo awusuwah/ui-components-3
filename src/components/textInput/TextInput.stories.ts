05aa563cd0463d0f581f964b6f24e0c83b0fdc73
Marko Maric <marko@markomaric.me>
2023-07-11 00:22:00 +0200

    Added support for loading in a TextInput and added a story for it. Also added a story rendering the TextInput with an icon in the start slot.

--- src/components/textInput/TextInput.stories.ts
+++ src/components/textInput/TextInput.stories.ts
@@ -34,3 +34,29 @@ export const Loading: Story = {
     loading: true,
   },
 };
+
+export const StartIcon: Story = {
+  render: (args) => ({
+    components: { TextInput },
+    setup() {
+      return {
+        ...args,
+      };
+    },
+    template: `
+      <TextInput label="Email Address">
+        <template #start>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 12a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+            <path fill-rule="evenodd"
+              d="M10 0a10 10 0 100 20 10 10 0 000-20zM1 10a9 9 0 1118 0 9 9 0 01-18 0z"
+              clip-rule="evenodd" />
+          </svg>
+        </template>
+      </TextInput>
+    `,
+  }),
+  args: {
+    label: "Email Address",
+  },
+};
--- src/components/textInput/TextInput.vue
+++ src/components/textInput/TextInput.vue
@@ -1,31 +1,46 @@
 <template>
-  <div class="rounded-md px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-thm">
-    <label for="name" class="block text-xs font-medium text-gray-900">{{ label }}</label>
-    <div class="flex gap-x-2">
-      <!-- Slot which renders content before the input element -->
-      <slot name="start">
-        <!-- Loading indicator -->
-        <svg v-if="loading" class="animate-spin w-6 h-6" fill="none" viewBox="0 0 24 24">
-          <circle class="text-thm-light opacity-25" cx="12" cy="12" r="10" stroke-width="4"></circle>
-          <path
-            class="text-thm opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          />
-        </svg>
-      </slot>
+  <div
+    :class="[
+      { 'pb-1.5 pt-2.5': !loading, 'py-2.5': loading },
+      'rounded-md h-14 px-3 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-thm',
+    ]"
+  >
+    <div v-if="!loading">
+      <label for="name" class="block text-xs font-medium text-gray-900">{{ label }}</label>
+      <div class="flex gap-x-2">
+        <!-- Slot which renders content before the input element -->
+        <slot name="start"> </slot>
 
-      <!-- Input element -->
-      <input
-        type="text"
-        name="name"
-        id="name"
-        class="block flex-1 border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 focus:outline-transparent sm:text-sm sm:leading-6"
-        placeholder="Jane Smith"
-      />
+        <!-- Input element -->
+        <input
+          type="text"
+          name="name"
+          id="name"
+          class="block flex-1 border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 focus:outline-transparent sm:text-sm sm:leading-6"
+          placeholder="Jane Smith"
+        />
 
-      <!-- Slot which renders content after the input element-->
-      <slot name="end"> </slot>
+        <!-- Slot which renders content after the input element-->
+        <slot name="end"> </slot>
+      </div>
+    </div>
+
+    <!-- ------------- -->
+    <!-- Loading state -->
+    <!-- ------------- -->
+    <div v-if="loading" class="h-full flex items-center gap-x-4">
+      <svg class="animate-spin w-8 h-8" fill="none" viewBox="0 0 24 24">
+        <circle class="text-thm-light opacity-25" cx="12" cy="12" r="10" stroke-width="4" />
+        <path
+          class="text-thm opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        />
+      </svg>
+
+      <span>
+        {{ loadingText }}
+      </span>
     </div>
   </div>
 </template>
@@ -51,6 +66,12 @@ const props = defineProps({
   loading: {
     type: Boolean,
     default: false,
+  },
+
+  // Render the text which is displayed next to the loading spinner while the TextInput is loading.
+  loadingText: {
+    type: String,
+    default: "Loading...",
   },
 });
 </script>
