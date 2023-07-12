8241c4cc9b5a0ff5f565f6f7fa8151d41b35ec77
Marko Maric <marko@markomaric.me>
2023-07-13 00:42:50 +0200

    Added the modal to the index.ts file so the component gets exported.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import Button from "./components/button/Button.vue";
-import TextInput from "./components/textInput/TextInput.vue";
+export { default as Button } from "./components/button/Button.vue";
+export { default as TextInput } from "./components/textInput/TextInput.vue";
+export { default as Modal } from "./components/modal/Modal.vue";
+
 import "./assets/tailwind.css";
 
-export { Button, TextInput };
+// import Button from "./components/button/Button.vue";
+
+// import TextInput from "./components/textInput/TextInput.vue";
+// import "./assets/tailwind.css";
+
+// export { Button, TextInput };
